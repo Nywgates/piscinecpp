3833a2702de97392d60d8f062ae95bc5eee79b66
Nywgates <aballea.louis@gmail.com>
2021-06-09 10:08:22 +0200

    ex02 done

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -12,6 +12,63 @@
 
 #include "mutantstack.hpp"
 
-int		main(){
-	
+int main() {
+    std::cout << "42 TEST" << std::endl;
+    MutantStack<int>    mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+
+    while(it != ite) {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+
+    std::cout << "My test" << std::endl;
+
+    MutantStack<int> s(mstack);
+
+    for (int i = 0; i < 42; i++) {
+        s.push(i);
+    }
+
+    std::cout << s.empty() << std::endl;
+    std::cout << s.size() << std::endl;
+    std::cout << s.top() << std::endl;
+
+    MutantStack<int>::reverse_iterator rit = s.rbegin();
+    MutantStack<int>::reverse_iterator rite = s.rend();
+
+    while(rit != rite) {
+        std::cout << *rit << std::endl;
+        ++rit;
+    }
+
+    MutantStack<int>::size_type size = s.size();
+
+    for (MutantStack<int>::size_type i = 0; i < size; i++) {
+        std::cout << s.top() << std::endl;
+        s.pop();
+    }
+
+    std::cout << s.empty() << std::endl;
+
+    return 0;
 }
